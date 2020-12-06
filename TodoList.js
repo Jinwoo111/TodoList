f2ae720179054f51107014ad9402c1cef7494f42
Unknown <ljw041101@gmail.com>
2020-12-06 22:52:15 +0900

    2020 12 06

--- TodoList.js
+++ TodoList.js
@@ -4,23 +4,23 @@ window.onload = function(){
     var ReviseButton = `<button class=\"Revise_Button\">수정하기</button>`
     var DeleteButton = `<button class=\"Delete_Button\">삭제하기</button>`
     Plus.onclick = function(){
-        var myTextArea = document.querySelector("[id=Header_TodoList]");
+        var myText = document.querySelector("[id=Header_TodoList]");
         var newList = document.createElement("div");
-        var removerStr = `<div class=\"TodoList_content\">${myTextArea.value}&nbsp;</div>`;
-        newList.innerHTML = removerStr + ReviseButton + DeleteButton;
+        var DeleteDiv = `<div class=\"TodoList_content\">${myText.value}&nbsp;</div>`;
+        newList.innerHTML = DeleteDiv + ReviseButton + DeleteButton;
         MainMenu.appendChild(newList);
-        myTextArea.value ="";
+        myText.value ="";
         Update();
 
         function Update(){
-            var myDelBtns=document.getElementsByClassName("Delete_Button"); 
-            var myRevBtns=document.getElementsByClassName("Revise_Button");
-            for(var i=0;i<myDelBtns.length;i++){
-                myDelBtns[i].addEventListener("click", Delete);
+            var DelBtns=document.getElementsByClassName("Delete_Button"); 
+            var RevBtns=document.getElementsByClassName("Revise_Button");
+            for(var i=0;i<DelBtns.length;i++){
+                DelBtns[i].addEventListener("click", Delete);
             }
-            for(var j=0;j<myRevBtns.length;j++){
-                if(myRevBtns[j].addEventListener("click", Revise) == true){
-                    myRevBtns[j].addEventListener("click", Revise);
+            for(var j=0;j<RevBtns.length;j++){
+                if(RevBtns[j].addEventListener("click", Revise) == true){
+                    RevBtns[j].addEventListener("click", Revise);
                     break;
                 }
             }
@@ -30,8 +30,8 @@ window.onload = function(){
             }
             function Revise(){
                 const Revise_sentent = prompt('수정할 내용을 입력하시오', '내용 입력하기');
-                var removerStr = `<div class=\"TodoList_content\">${Revise_sentent}&nbsp;</div>`;
-                newList.innerHTML = removerStr + ReviseButton + DeleteButton;
+                var DeleteDiv = `<div class=\"TodoList_content\">${Revise_sentent}&nbsp;</div>`;
+                newList.innerHTML = DeleteDiv + ReviseButton + DeleteButton;
             }
         }
     }
