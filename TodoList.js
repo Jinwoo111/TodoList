596575cfc580f54cbe37971c58b891f8ca44e95d
Unknown <ljw041101@gmail.com>
2021-01-03 00:10:42 +0900

    2021 01 03

--- TodoList.js
+++ TodoList.js
@@ -8,7 +8,7 @@ window.onload = function(){
         var newList = document.createElement("div");
         var DeleteDiv = `<div class=\"TodoList_content\">${myText.value}&nbsp;</div>`;
         newList.innerHTML = DeleteDiv + ReviseButton + DeleteButton;
-        MainMenu.appendChild(newList);
+        MainMenu.appendChild(newList);  
         myText.value ="";
         Update();
 
